ca72145097ed1e6a75a6db99cdb14ae18047a844
SunnyMaddineni <43933913+SunnyMaddineni@users.noreply.github.com>
2020-08-24 16:05:28 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+
+import TabNavigator from './components/Navigator';
+
+export default function App() {
+  return (
+   <TabNavigator/>
+  );
+}
+
+
