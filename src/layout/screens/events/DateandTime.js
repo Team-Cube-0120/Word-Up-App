5b6e1fd0e06ac810a43041e0dc12b04ea6b2dac3
SunnyMaddineni <smaddineni@yahoo.com>
2020-09-06 14:04:29 -0400

    {Sprint 1 - Events]: date and time revert

--- src/layout/screens/events/DateandTime.js
+++ src/layout/screens/events/DateandTime.js
@@ -4,110 +4,89 @@ import DateTimePicker from '@react-native-community/datetimepicker';
 import PropTypes from 'prop-types';
 
 
-class DateandTime extends Component {
-    static propTypes = {
-        onStartDateSelection: PropTypes.func,
-        onStartTimeSelection: PropTypes.func,
-        onEndDateSelection: PropTypes.func,
-        onEndTimeSelection: PropTypes.func,
-    }
 
+
+const DateandTime = () => {
 
     onChange = (event, selectedDate) => {
         const currentDate = selectedDate || date;
         alert(currentDate);
-        
+
         setShow(Platform.OS === 'ios');
         setDate(currentDate);
     };
 
-    showMode = (currentMode) => {
+
+    const showMode = (currentMode) => {
         setShow(true);
         setMode(currentMode);
     };
 
-    showDatepicker = () => {
+    const showDatepicker = () => {
         showMode('date');
     };
 
-    showTimepicker = () => {
+    const showTimepicker = () => {
         showMode('time');
     };
-    // const [date, setDate] = useState(new Date(1598051730000));
-    // const [mode, setMode] = useState('date');
-    // const [show, setShow] = useState(false);
-    render() {
-        return (
-            <View>
-                <View style={{ flexDirection: 'row' }}>
-                    <Text style={styles.text}>Start Date:</Text>
-                    <TouchableOpacity style={styles.button}
-                        onPress={this.showDatepicker}>
-                        <Text style={styles.btnText}>Calender</Text>
-                    </TouchableOpacity>
+    const [date, setDate] = useState(new Date(1598051730000));
+    const [mode, setMode] = useState('date');
+    const [show, setShow] = useState(false);
+    return (
+        <View>
+            <View style={{ flexDirection: 'row' }}>
+                <Text style={styles.text}>Start Date:</Text>
+                <TouchableOpacity style={styles.button}
+                    onPress={showDatepicker}>
+                    <Text style={styles.btnText}>Calender</Text>
+                </TouchableOpacity>
 
-                    <Text style={styles.text}>Start Time:</Text>
-                    <TouchableOpacity style={styles.button}
-                        onPress={this.showTimepicker}>
-                        <Text style={styles.btnText}>Clock</Text>
-                    </TouchableOpacity>
-                </View>
-                <View>
-                    <Text></Text>
-                </View>
 
-                <View style={{ flexDirection: 'row' }}>
-                    <Text style={styles.text}>End Date:</Text>
-                    <TouchableOpacity style={styles.button}
-                        onPress={this.showDatepicker}>
-                        <Text style={styles.btnText}>Calender</Text>
-                    </TouchableOpacity>
-
-                    <Text style={styles.text}>End Time:</Text>
-                    <TouchableOpacity style={styles.button}
-                        onPress={this.showTimepicker}>
-                        <Text style={styles.btnText}>Clock</Text>
-                    </TouchableOpacity>
-                </View>
-                <View>
-
-                </View>
-                    <DateTimePicker
-                        testID="dateTimePicker"
-                        value={date}
-                        mode={mode}
-                        is24Hour={true}
-                        display="default"
-                        onChange={onChange}
-                    />
-                    <DateTimePicker
-                        testID="dateTimePicker"
-                        value={date}
-                        mode={mode}
-                        is24Hour={true}
-                        display="default"
-                        onChange={onChange}
-                    />
-                    <DateTimePicker
-                        testID="dateTimePicker"
-                        value={date}
-                        mode={mode}
-                        is24Hour={true}
-                        display="default"
-                        onChange={onChange}
-                    />
-                    <DateTimePicker
-                        testID="dateTimePicker"
-                        value={date}
-                        mode={mode}
-                        is24Hour={true}
-                        display="default"
-                        onChange={onChange}
-                    />
+                <Text style={styles.text}>Start Time:</Text>
+                <TouchableOpacity style={styles.button}
+                    onPress={showTimepicker}>
+                    <Text style={styles.btnText}>Clock</Text>
+                </TouchableOpacity>
             </View>
-        )
-    }
+            <View>
+                <Text></Text>
+            </View>
+
+
+            <View style={{ flexDirection: 'row' }}>
+                <Text style={styles.text}>End Date:</Text>
+                <TouchableOpacity style={styles.button}
+                    onPress={showDatepicker}>
+                    <Text style={styles.btnText}>Calender</Text>
+                </TouchableOpacity>
+
+
+                <Text style={styles.text}>End Time:</Text>
+                <TouchableOpacity style={styles.button}
+                    onPress={showTimepicker}>
+                    <Text style={styles.btnText}>Clock</Text>
+                </TouchableOpacity>
+            </View>
+            <View>
+
+            </View>
+            {show && (
+                <DateTimePicker
+                    testID="dateTimePicker"
+                    value={date}
+                    mode={mode}
+                    is24Hour={true}
+                    display="default"
+                    onChange={onChange}
+                />
+            )}
+
+        </View>
+
+    )
 }
+
+
 
 const styles = StyleSheet.create({
     text: {
@@ -127,5 +106,4 @@ const styles = StyleSheet.create({
         fontWeight: "bold"
     },
 });
-
 export default DateandTime;
