2803111ca9242342bceebb1666d7408bfb23e99d
Naim Kabir <abunaimkabir@gmail.com>
2020-10-28 01:28:29 -0400

    Persist Login Added and merged sign in with Jobs

--- App.js
+++ App.js
@@ -1,10 +1,9 @@
-import React, {Component, useState} from 'react';
+import React, {useState} from 'react';
 import TabNavigator from './src/components/navigation/Navigator'
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import LoginStackScreen from './src/components/navigation/LoginStackNavigator';
-import { storeData, getData } from './src/util/LocalStorage'
-import { firebase } from "./server/config/firebase/firebaseConfig";
+import { getData } from './src/util/LocalStorage'
 import { USERINFO } from './src/enums/StorageKeysEnum';
 
 const Stack = createStackNavigator();
@@ -16,50 +15,13 @@ const Stack = createStackNavigator();
 //   }
 // }
 
-class App extends Component {
-
-  autoLogin() {
-    firebase
-      .auth()
-      .signInWithEmailAndPassword('test1@gmail.com', 'password')
-      .then((response) => {
-        const uid = response.user.uid;
-        const usersRef = firebase.firestore().collection("users");
-        usersRef
-          .doc(uid)
-          .get()
-          .then(async (firestoreDocument) => {
-            if (!firestoreDocument.exists) {
-              alert("User does not exist anymore.");
-              return;
-            } else {
-              await storeData(USERINFO, firestoreDocument.data());
-            }
-          })
-          .catch((error) => {
-            alert(error);
-          });
-      })
-      .catch((error) => {
-        alert(error);
-      });
-  }
-
-  componentDidMount() {
-    this.autoLogin();
-  }
-
-  render() {
-    return (
-      <NavigationContainer>
-        <Stack.Navigator>
-          {/* <Stack.Screen name="LoginStackNavigator" component={LoginStackScreen} options={{ headerShown: false }} /> */}
-          <Stack.Screen name="TabNavigator" component={TabNavigator} options={{ headerShown: false }} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    );
-  }
-
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="LoginStackNavigator" component={LoginStackScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="TabNavigator" component={TabNavigator} options={{ headerShown: false }} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
-
-export default App;
--- src/enums/ApiConfigurationEnum.js
+++ src/enums/ApiConfigurationEnum.js
@@ -1,6 +1,6 @@
 const ApiConfigurationsEnum = {
-    MOCK_SERVICE_URL: 'http://10.0.0.171:8000/'
-    // MOCK_SERVICE_URL: 'http://10.0.2.2:8000/'
+    // MOCK_SERVICE_URL: 'http://10.0.0.171:8000/'
+    MOCK_SERVICE_URL: 'http://10.0.2.2:8000/'
 }
 
 module.exports = ApiConfigurationsEnum;
